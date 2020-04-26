14252b1a1fbb14144198b2a63a8ca3e62c215ed0
Ben Moss <benm@vmware.com>
2020-04-26 21:55:41 +0000

    Add dockerfile, allow port to be customized

--- main.go
+++ main.go
@@ -16,7 +16,14 @@ func main() {
 	http.HandleFunc("/call", CallHandler)
 	http.HandleFunc("/record", RecordingHandler)
 	http.HandleFunc("/transcription", TranscriptionHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("listening on %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
 
 type Inbound struct {
